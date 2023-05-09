108bf03c80d43556701e2fe4f102836c5ec0461e
sarahmarkland <sarah.markland@icloud.com>
2023-05-09 15:27:40 -0700

    task 1 pep8 probz

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,13 +4,13 @@ number = random.randint(-10000, 10000)
 last_digit = (abs(number) % 10)
 str1 = "Last digit of "
 str2 = "is "
-greater_str = "and is greater than 5"
+great_str = "and is greater than 5"
 zero_str = "and is 0"
 less_str = "and is less than 6 and not 0"
 if(number < 0):
     last_digit *= -1
 if last_digit > 5:
-    print(str1 + str(number) + " " + str2 + str(last_digit) + " " + greater_str)
+    print(str1 + str(number) + " " + str2 + str(last_digit) + " " + great_str)
 elif last_digit == 0:
     print(str1 + str(number) + " " + str2 + str(last_digit) + " " + zero_str)
 else:
