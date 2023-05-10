829dd4ce4fc29701ca263f8140c127be0b93f4a4
sarahmarkland <sarah.markland@icloud.com>
2023-05-10 12:35:05 -0700

    oops. did task 9 before task 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number < 0:
-        number = number * -1
-    last_digit = number % 10
-    print("{:d}".format(last_digit), end="")
-    return last_digit
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+        print("{}".format(i), end="")
+    print("")
