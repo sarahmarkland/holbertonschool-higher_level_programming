469a2fab48a570f19edd51819d90cf22f78ba70a
sarahmarkland <sarah.markland@icloud.com>
2023-05-12 12:53:46 -0500

    remove for loop

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for item in my_list:
-        if idx < 0 or idx >= len(my_list):
-            return None
-        else:
-            return item
+    if idx < 0 or idx >= len(my_list):
+        return None
+    else:
+        return item
