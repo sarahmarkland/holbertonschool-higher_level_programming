f05bb96d9285b525aebe32810dd23be7483e4d05
sarahmarkland <sarah.markland@icloud.com>
2023-05-15 14:29:07 -0500

    task 7 commit 2, handle < 2

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     # Extract the first two elements from each tuple (or use 0 if the tuple is smaller)
-    a1, a2 = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
-    b1, b2 = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
+    a1, a2 = tuple_a[:2] + (0, 0)[len(tuple_a):2]
+    b1, b2 = tuple_b[:2] + (0, 0)[len(tuple_b):2]
 
     # Perform the addition of the corresponding elements
     sum_1 = a1 + b1
