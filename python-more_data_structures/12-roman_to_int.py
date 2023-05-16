54feae67f4911fd7e18cc48807795c79db1c8b3d
sarahmarkland <sarah.markland@icloud.com>
2023-05-16 17:29:30 -0500

    task 12 commit kl;lj

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    if len(a_dictionary) == 0:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
