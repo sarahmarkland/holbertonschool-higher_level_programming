597bb178d96810bce65e22ce52de637bfd786bb2
sarahmarkland <sarah.markland@icloud.com>
2023-05-16 21:18:08 -0500

    task 0 commit 1

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for i in my_list:
+            if count < x:
+                print(i, end='')
+                count += 1
+            else:
+                break
+        print()
+        return count
+    except:
+        return count
