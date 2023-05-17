58700680dd65941fb4ed0be1a28e59b026ea6942
sarahmarkland <sarah.markland@icloud.com>
2023-05-16 23:50:13 -0500

    pepeeeeeee

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,8 +6,10 @@ def roman_to_int(roman_string):
                   'C': 100, 'D': 500, 'M': 1000}
     num = 0
     for i in range(len(roman_string)):
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            num += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        if i > 0 and roman_dict[roman_string[i]] >
+        roman_dict[roman_string[i - 1]]:
+            num += roman_dict[roman_string[i]] - 2 *
+            roman_dict[roman_string[i - 1]]
         else:
             num += roman_dict[roman_string[i]]
     return num
