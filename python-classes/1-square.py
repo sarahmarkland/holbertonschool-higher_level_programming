18fee8a0da5afeab7ba9cfa7f7e8ecaa80dc91c1
sarahmarkland <sarah.markland@icloud.com>
2023-05-18 12:53:39 -0500

    task 1 commit 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,5 +4,9 @@
 
 class Square:
     """This class defines a square"""
-    def __init__(self, size):
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
