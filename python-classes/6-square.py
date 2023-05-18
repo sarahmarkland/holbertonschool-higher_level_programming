415261741986b487292de1004bf4f88ed8e1c55b
sarahmarkland <sarah.markland@icloud.com>
2023-05-18 17:11:18 -0500

    task 6 why am i trying to break this

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,18 +52,3 @@ class Square:
                 for k in range(self.size):
                     print("#", end="")
                 print()
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
