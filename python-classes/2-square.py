f378b853ed90750c9f3fadb7e8c04f3eb2247c5f
sarahmarkland <sarah.markland@icloud.com>
2023-05-18 12:55:10 -0500

    task 2 commit 1 - accidentally added to task 1

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write an empty class that defines a square"""
+
+
+class Square:
+    """This class defines a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
