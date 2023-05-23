dc952eeb26fa864e75a4f5dddc4c627e6a452b58
sarahmarkland <sarah.markland@icloud.com>
2023-05-23 16:37:33 -0500

    task 5 fix documentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
-"""3. Text indentation - Write func that prints text with 2 new line after: .,?:"""
+"""4. Text indentation - 5-text_indentation.py"""
 
 
 def test_indentation(text):
-    """test_indentation: func that prints text with 2 new line after: .,?:"""
-    if type(text) is not str:
+    """test_indentation: function that prints a text with 2 new lines after
+    each of these characters: ., ? and :
+    Args:
+        text (str): text to print
+    Returns:
+        nothing
+    Raises:
+        TypeError: if text is not a string
+    Doctest Examples:
+        see dir: /tests/5-text_indentation.txt
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
+    new_text = ""
     for i in range(len(text)):
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
-            print(text[i])
-            print()
+            new_text += text[i]
+            new_text += "\n\n"
         else:
-            print(text[i], end="")
+            new_text += text[i]
+    print(new_text, end="")
