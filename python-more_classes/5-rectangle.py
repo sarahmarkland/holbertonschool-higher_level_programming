0f873310d7b1682beb6dd644dd0a57cbe08d4754
sarahmarkland <sarah.markland@icloud.com>
2023-05-23 20:53:27 -0500

    task 5 commit 1

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+Class Rectangle that defines a rectangle by:
+    Private instance attribute: width
+    Private instance attribute: height
+    Instantiation with optional width and height: def __init__
+    (self, width=0, height=0):
+    Public instance method: def area(self): that returns the
+    rectangle area
+    Public instance method: def perimeter(self): that returns the
+    rectangle perimeter:
+    print() and str() should print the rectangle with the char #:
+        if width or height is equal to 0, return an empty string
+    repr() should return a string representation of the rectangle
+    to be able to recreate a new instance by using eval()
+    Print the message Bye rectangle... (... being 3 dots not ellipsis)
+"""
+
+
+class Rectangle:
+    """Initializes the Rectangle data"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    """Getter for private attribute width"""
+    @property
+    def width(self):
+        return self.__width
+
+    """Setter for private attribute width"""
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = int(value)
+
+    """Getter for private attribute height"""
+    @property
+    def height(self):
+        return self.__height
+
+    """Setter for private attribute height"""
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = int(value)
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            perimeter = (self.width * 2) + (self.height * 2)
+            return perimeter
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            string = ""
+            for i in range(self.height):
+                string += "#" * self.width
+                if i < self.height - 1:
+                    string += "\n"
+            return string
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
