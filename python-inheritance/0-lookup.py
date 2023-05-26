54a4c0e8fb812a0f487bee761e95b1886dee541a
sarahmarkland <sarah.markland@icloud.com>
2023-05-26 13:12:30 -0500

    task 0 comm 3

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,10 +3,12 @@
 Return: a list of available attributes and methods of
     an object
 """
+
+
 def lookup(obj):
     """
     Retrieve the list of attributes and methods associated
-    with the given object obj. This func returns sorted list
+    with the given object. This func returns sorted list
     of strings containing the names of the attributes and methods.
     """
     return dir(obj)
