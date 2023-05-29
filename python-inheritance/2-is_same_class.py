3e0cfd8a63b1f9b6532ad554303186a464337b33
sarahmarkland <sarah.markland@icloud.com>
2023-05-29 13:10:19 -0500

    task 2 checking pepe with rulers setup

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,7 +8,7 @@ instance of the specified class ; otherwise False.
 
 def is_same_class(obj, a_class):
     """
-    
+
     Check if an object is exactly an instance of the specified class.
 
     Args:
