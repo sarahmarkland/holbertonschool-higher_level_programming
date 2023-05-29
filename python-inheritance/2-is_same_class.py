3e5d832597679ea3b7fe02bd37e77afd9009e885
sarahmarkland <sarah.markland@icloud.com>
2023-05-29 12:45:52 -0500

    task 3

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
-module documentation
+Module documentation
+Write a function that returns True if the object is exactly an
+instance of the specified class ; otherwise False.
 """
 
 
 def is_same_class(obj, a_class):
     """
+    
     Check if an object is exactly an instance of the specified class.
 
     Args:
---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module documentation:
+Write a function that returns True if the object is an
+instance of, or if the object is an instance of a class
+that inherited from, the specified class ; otherwise False.
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if an object is an instance of, or if the object is an instance of a class
+    that inherited from, the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        True if the object is an instance of, or if the object is an instance of a class
+        that inherited from, the specified class; otherwise, False.
+    """
+    return isinstance(obj, a_class) or issubclass(type(obj), a_class)
