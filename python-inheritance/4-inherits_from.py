2c77c9d197d9d7b127f7fb55d1188ca41ba0cb76
sarahmarkland <sarah.markland@icloud.com>
2023-05-29 15:17:32 -0500

    task 4 pepe

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -17,8 +17,8 @@ def inherits_from(obj, a_class):
         a_class: The class to compare against.
 
     Returns:
-        True if the object is an instance of a class that inherited 
-        (directly or indirectly) from the specified class; otherwise, 
+        True if the object is an instance of a class that inherited
+        (directly or indirectly) from the specified class; otherwise,
         False.
     """
     return isinstance(obj, a_class) and type(obj) != a_class
