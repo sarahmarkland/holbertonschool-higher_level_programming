935f4104e230a312e761d3a763a785a7aeafefaa
sarahmarkland <sarah.markland@icloud.com>
2023-05-29 12:31:48 -0500

    task 2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    method documentation
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
