2c41c6a5bdd59563dbeb7093d6a890756f33c851
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 15:59:25 -0500

    task 11 mod doc issues, add import

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -3,6 +3,7 @@
 Write a class Square that inherits from Rectangle (9-rectangle.py).
 (task based on 10-square.py).
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
