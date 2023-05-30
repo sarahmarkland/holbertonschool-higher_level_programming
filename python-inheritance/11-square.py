2ed997a955d944f6a823582dde431f4758fc7ee9
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 13:29:04 -0500

    task 11

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+
+class BaseGeometry:
+    """
+    class BaseGeometry
+    """
+    def area(self):
+        """
+        function area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function integer_validator
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
