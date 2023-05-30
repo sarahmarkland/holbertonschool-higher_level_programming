1da9247f2d8233fe7fc9e33f72ad884c8f63fc60
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 18:25:30 -0500

    task 7 mess

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" 7-add_item.py"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+try:
+    items = load_from_json_file("add_item.json")
+except:
+    items = []
+for i in range(1, len(sys.argv)):
+    items.append(sys.argv[i])
+save_to_json_file(items, "add_item.json")
