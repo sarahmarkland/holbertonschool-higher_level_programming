def68dd688ba1e96116cd2a8c77e72c21c6c60cf
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 16:28:37 -0500

    task 6 fix

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -7,6 +7,19 @@ write an empty class BaseGeometry
 
 class BaseGeometry:
     """
-    class documentation
+    class BaseGeometry
     """
-    pass
+    def area(self):
+        """
+        function area
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        function integer_validator
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
