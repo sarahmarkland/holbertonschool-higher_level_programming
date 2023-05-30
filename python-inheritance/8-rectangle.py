f7285d53b14f4e3dbdc8e315bbf13600265e7111
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 13:17:04 -0500

    task 9

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,6 +3,8 @@
 module documentation
 write a class Rectangle that inherits from BaseGeometry
 """
+
+
 class BaseGeometry:
     """
     class BaseGeometry
@@ -22,6 +24,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """
     class Rectangle
@@ -34,7 +37,7 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     def __str__(self):
         """
         function __str__
