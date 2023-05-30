9367dec5c4b43028db58a65344be099f7e75f706
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 12:34:31 -0500

    task 7 with isinstance

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry:
         """
         function integer_validator
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
