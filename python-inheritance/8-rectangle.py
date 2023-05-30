7a593c0e1268acb8e3d6582be7e1363a037799f5
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 16:04:38 -0500

    task 8 fix import issue

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -3,26 +3,7 @@
 module documentation
 write a class Rectangle that inherits from BaseGeometry
 """
-
-
-class BaseGeometry:
-    """
-    class BaseGeometry
-    """
-    def area(self):
-        """
-        function area
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        function integer_validator
-        """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -37,15 +18,3 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        """
-        function __str__
-        """
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        """
-        function area
-        """
-        return self.__width * self.__height
