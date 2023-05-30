002291551dfb05a2ccd2aff480085222040b376c
sarahmarkland <sarah.markland@icloud.com>
2023-05-30 16:33:06 -0500

    mission fix 6

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -14,12 +14,3 @@ class BaseGeometry:
         function area
         """
         raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """
-        function integer_validator
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
