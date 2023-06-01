3cfde840d48014688d6b4dbdd7fded0361919f70
sarahmarkland <sarah.markland@icloud.com>
2023-06-01 17:28:37 -0500

    task 5 go go

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -6,7 +6,7 @@ from models.base import Base
 class Rectangle(Base):
     """ Rectangle class """
     def __init__(self, width, height, x=0, y=0, id=None):
-        """ Initialize Rectangle class """
+        """ Initialize Rectangle class update meh """
         super().__init__(id)
         self.width = width
         self.height = height
