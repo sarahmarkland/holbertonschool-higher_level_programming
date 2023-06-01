ef1e1e964adb20b3ee3778630795dbe33256a336
sarahmarkland <sarah.markland@icloud.com>
2023-06-01 16:21:06 -0500

    task 1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+""" Rectangle class """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle class """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Initialize Rectangle class """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Return width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set width """
+        self.__width = value
+        self.validate_width(value)
+
+    @property
+    def height(self):
+        """ Return height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set height """
+        self.__height = value
+        self.validate_height(value)
+
+    @property
+    def x(self):
+        """ Return x """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ Set x """
+        self.__x = value
+        self.validate_x(value)
+
+    @property
+    def y(self):
+        """ Return y """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ Set y """
+        self.__y = value
+        self.validate_y(value)
