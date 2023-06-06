0a9c5ceb04831532628a417ae5fca3aeabad29af
sarahmarkland <sarah.markland@icloud.com>
2023-06-05 23:12:39 -0500

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Square class, inherit from Rectangle'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''initialize Square class'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''return string representation of Square'''
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
