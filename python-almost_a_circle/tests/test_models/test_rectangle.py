61ac36783e235981c973e6d63c46e1c07ea2f4c0
sarahmarkland <sarah.markland@icloud.com>
2023-06-06 15:49:38 -0500

    push with last test failing

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -273,5 +273,19 @@ class TestRectangle(unittest.TestCase):
             self.assertEqual(saved_data[0]['x'], r1.x)
             self.assertEqual(saved_data[0]['y'], r1.y)
 
+    @patch('builtins.open', new_callable=MagicMock)
+    def test_load_from_file_file_not_found(self, mock_open):
+        mock_open.side_effect = FileNotFoundError
+        mock_open.return_value.__enter__.return_value.read.return_value = ""
+
+        # Call the load_from_file method
+        result = Rectangle.load_from_file()
+
+        # Assert that open was called with the correct filename
+        mock_open.assert_called_once_with('Rectangle.json', 'r', encoding='utf-8')
+
+        # Assert that the result is an empty list
+        self.assertEqual(result, [])
+
 if __name__ == '__main__':
     unittest.main()
