6c77a8b17daf855305ca912190ecdf5eb16b8659
sarahmarkland <sarah.markland@icloud.com>
2023-06-05 23:30:36 -0500

    task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
-    # @staticmethod
+    @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
         if list_dictionaries is None or list_dictionaries == []:
