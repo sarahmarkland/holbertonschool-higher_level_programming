e506f28c7b2120aa031e036bf7c72bf7f67bfc70
sarahmarkland <sarah.markland@icloud.com>
2023-06-27 14:28:31 -0500

    task 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SELECT user, host, Grant_priv, Super_priv FROM mysql.user;
+SELECT GRANTS FOR 'user_0d_1'@'localhost';
+SELECT GRANTS FOR 'user_0d_2'@'localhost';
