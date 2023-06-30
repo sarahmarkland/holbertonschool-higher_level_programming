0bf298cfd83f34086af4d7c465c3b3029fc7ad16
sarahmarkland <sarah.markland@icloud.com>
2023-06-30 13:47:26 -0500

    task 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- imports the database dump from hbtn_0d_tvshows to your MySQL server: hbtn_0d_tvshows
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
