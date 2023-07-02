74ac37e28ce0d56dabfa05741f89b9baeb1893f1
sarahmarkland <sarah.markland@icloud.com>
2023-07-02 18:57:35 -0500

    copy ben's code for fun

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" Script that lists all states from the database hbtn_0e_0_usa idk whats going on"""
+"""
+Write a script that lists all states from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
 
-def listStates():
-    """ Function that lists all states from the database hbtn_0e_0_usa """
+def list_states():
+    """List the states"""
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-    
+
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
@@ -21,4 +23,4 @@ def listStates():
 
 
 if __name__ == "__main__":
-    listStates()
+    list_states()
