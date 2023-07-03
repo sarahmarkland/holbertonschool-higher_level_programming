76af7c3a5ebf7ff5e0c8c431bf953511be431fe3
sarahmarkland <sarah.markland@icloud.com>
2023-07-03 13:53:11 -0500

    task 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """ Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa """
-
-
 import MySQLdb
-import sys
+from sys import argv
 
 
 def listNStates():
