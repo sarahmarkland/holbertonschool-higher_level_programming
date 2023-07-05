edd9f5a6d4796c596ce9df16c8fa1856b7aea8e6
sarahmarkland <sarah.markland@icloud.com>
2023-07-05 18:36:07 -0500

    task 5

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that takes in the name of a state as an argument and lists all cities of
+that state, using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
+
+
+def filterCities():
+    """ Script that takes in the name of a state as an argument and lists all
+    cities of that state, using the database hbtn_0e_4_usa """
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states ON \
+                cities.state_id = states.id WHERE states.name = %s \
+                ORDER BY cities.id ASC", (argv[4],))
+    query_rows = cur.fetchall()
+    print(", ".join([row[0] for row in query_rows]))
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    filterCities()
