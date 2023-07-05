f28b5d0f3aa58a7f9765b3dc1498e11df56c11bb
sarahmarkland <sarah.markland@icloud.com>
2023-07-05 18:06:44 -0500

    task 2 v 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -15,10 +15,14 @@ def filterStates():
 
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE BINARY name LIKE '{}' ORDER BY id"
-                .format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                ORDER BY id.".format(argv[4]))
     states = cur.fetchall()
     for state in states:
         print(state)
     cur.close()
     db.close()
+
+
+if __name__ == "__main__":
+    filterStates()
