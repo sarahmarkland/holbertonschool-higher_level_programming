745a33088c3918e412737baf0a7d469311f3fd2c
sarahmarkland <sarah.markland@icloud.com>
2023-07-05 18:03:56 -0500

    task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Takes in an argument and displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+
+def filterStates():
+    """ filter states by user input """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name LIKE '{}' ORDER BY id"
+                .format(argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
