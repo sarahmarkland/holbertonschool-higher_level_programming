ea212c84efe6f85445abc9d7f1c836a252164c45
sarahmarkland <sarah.markland@icloud.com>
2023-08-01 11:46:20 -0700

    task 1

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,4 @@
 #!/usr/bin/node
-// print "JavaScript is amazing" //
-console.log("JavaScript is amazing")
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
