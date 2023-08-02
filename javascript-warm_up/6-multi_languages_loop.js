8f4e8ec83954a7df29170c04dc875fdee9dc3bbf
sarahmarkland <sarah.markland@icloud.com>
2023-08-02 13:01:54 -0700

    task 6 fix

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const stringArr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const stringArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < stringArr.length; i++) {
   console.log(stringArr[i]);
 }
