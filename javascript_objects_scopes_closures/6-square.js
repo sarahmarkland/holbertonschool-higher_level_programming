5f4dd7cc50eaf65dfdc2ace17dd411c3a088adc8
sarahmarkland <sarah.markland@icloud.com>
2023-08-08 18:02:19 -0700

    oops 6 was done in main instead of square file

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Square = require('./5-square');
+
+class Square extends square {
+    constructor (size) {
+        super(size, size);
+}
+    charPrint (c) {
+        if (c === undefined) {
+            c = 'X';
+        }
+        for (let i = 0; i < this.height; i++) {
+            console.log(c.repeat(this.width));
+        }
+    }
+}
+module.exports = Square;
