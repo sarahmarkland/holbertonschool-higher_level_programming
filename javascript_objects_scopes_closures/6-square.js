00d4c6da7da792269b33c72dd68ae98f0044173e
sarahmarkland <sarah.markland@icloud.com>
2023-08-08 18:10:13 -0700

    task 6/7 semi

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,16 +2,17 @@
 const square = require('./5-square');
 
 class Square extends square {
-    constructor (size) {
-        super(size, size);
-}
-    charPrint (c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let i = 0; i < this.height; i++) {
-            console.log(c.repeat(this.width));
-        }
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 }
 module.exports = Square;
