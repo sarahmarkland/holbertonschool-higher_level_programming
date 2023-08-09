1780d030400cbb2df7249ebd621210f50e9c8ee6
sarahmarkland <sarah.markland@icloud.com>
2023-08-08 18:36:22 -0700

    task 4 my lyfe

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,16 +1,4 @@
 #!/usr/bin/node
-// Write a class Rectangle that defines a rectangle:
-// You must use the class notation for defining your class
-// The constructor must take 2 arguments w and h
-// If w or h is equal to 0 or not a positive integer, create an empty
-// object
-// Create an instance method called print() that prints the rectangle
-// using the character X
-// Create an instance method called rotate() that exchanges the width
-// and the height of the rectangle
-// Create an instance method called double() that multiples the width
-// and the height of the rectangle by 2
-
 class Rectangle {
   constructor (w, h) {
     if (w >= 1 && h >= 1) {
@@ -23,10 +11,10 @@ class Rectangle {
     let i = 0;
     let j = 0;
     let string = '';
-    for (; i < this.height; i++) {
+    for (; i < this.width; i++) {
       string += 'X';
     }
-    for (; j < this.width; j++) {
+    for (; j < this.height; j++) {
       console.log(string);
     }
   }
