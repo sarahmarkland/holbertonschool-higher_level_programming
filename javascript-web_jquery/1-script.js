9764ff68b842af6ebdb9f791aa09e3c9f34530ff
sarahmarkland <sarah.markland@icloud.com>
2023-08-10 20:49:24 -0500

    task 1

---
+++ javascript-web_jquery/1-script.js
@@ -0,0 +1,11 @@
+// Include the jQuery library (make sure to adjust the path to your jQuery file)
+// <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+// Wait for the document to be ready
+$(document).ready(function() {
+// Select the <header> element using the jQuery selector
+var headerElement = $("header");
+
+// Update the text color to red (#FF0000)
+headerElement.css("color", "#FF0000");
+}
+);
